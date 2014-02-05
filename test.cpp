2a6b0862902023179b801a4d1e6c524f87a184c3
Conrad Weidenkeller <conrad@weidenkeller.com>
2014-02-05 09:55:35 -0600

    Removed comments

--- test.cpp
+++ test.cpp
@@ -9,25 +9,23 @@ int main(int argc, char* argv[])
     RedisClient client = RedisClient("localhost", "6379", "conrad");
     redis_response res = client.ping();
     sleep(2);
-    //cout << res.status << endl<< res.data << endl;
+    cout << res.status << endl<< res.data << endl;
     res = client.bgsave();
     sleep(2);
-    //cout << res.status << endl<< res.data << endl;
+    cout << res.status << endl<< res.data << endl;
     res = client.save();
     sleep(2);
-    //cout << res.status << endl<< res.data << endl;
+    cout << res.status << endl<< res.data << endl;
     sleep(2);
     res = client.last_save();
-    //cout << res.status << endl<< res.data << endl;
+    cout << res.status << endl<< res.data << endl;
     sleep(2);
     res = client.config_get("requirepass");
-    //cout << res.status << endl<< res.data << endl;
+    cout << res.status << endl<< res.data << endl;
     sleep(2);
-    res = client.config_set("requirepass", "password");
-    //cout << res.status << endl<< res.data << endl;
+    res = client.config_set("requirepass", "foo");
+    cout << res.status << endl<< res.data << endl;
     sleep(2);
     res = client.config_rewrite();
-    //cout << res.status << endl<< res.data << endl;
-
-
+    cout << res.status << endl<< res.data << endl;
 }
