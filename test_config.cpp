3e8a247d4d0b22c34928394f7eee33bf7d5391bb
Conrad Weidenkeller <conrad@weidenkeller.com>
2014-03-02 06:31:48 -0600

    bug

--- test_config.cpp
+++ test_config.cpp
@@ -5,7 +5,7 @@
 using namespace nova::redis;
 int main()
 {
-    std::string path = "/home/cweidenkeller/red_gorgon/test_redis.conf";
+    std::string path = "test_redis.conf";
     Config conf(path);
     std::cout << conf.get_include_files().size() << std::endl;
     std::cout << conf.get_include_files()[0] << std::endl;
